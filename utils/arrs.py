4bf449b68c4c882ed8eaca095f19964706af8824
s.leontev <neonyan15@gmail.com>
2023-09-06 14:28:11 +0300

    fix functions

--- utils/arrs.py
+++ utils/arrs.py
@@ -11,13 +11,13 @@ def get(array, index, default=None):
     :param default: значение по-умолчанию.
     :return: значение по индексу или значение по-умолчанию.
     """
-    if index < 0:
-        return default
 
-    return array[index]
+    if 0 <= index < len(array):
+        return array[index]
+    return default
 
 
-def my_slice(coll, start=0, end=None):
+def my_slice(coll, start=None, end=None):
     """
     Возвращает новый массив, содержащий копию части исходного массива.
     :param coll: исходный список.
@@ -27,19 +27,20 @@ def my_slice(coll, start=0, end=None):
     Если индекс отрицательный, end указывает смещение от конца списка. По умолчанию равен длине исходного списка.
     :return: массив элементов
     """
+
     length = len(coll)
 
     if length == 0:
         return []
 
-    normalized_end = length if end is None else end
+    if start is None:
+        normalized_start = 0
+    else:
+        normalized_start = start
 
-    normalized_start = start
-
-    if normalized_start < 0:
-        if normalized_start < -length:
-            normalized_start = 0
-        else:
-            normalized_start += length
+    if end is None or end > length:
+        normalized_end = length
+    else:
+        normalized_end = end
 
     return coll[normalized_start:normalized_end]
