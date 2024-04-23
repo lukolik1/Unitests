b144415dc7f592c1ed0d6d86306dcbe3b9345249
lukolik1 <111924837+lukolik1@users.noreply.github.com>
2024-04-23 19:43:55 +0300

    Update main -force

---
+++ tests/test_arrs.py
@@ -0,0 +1,14 @@
+import unittest
+from utils import arrs
+
+
+class TestArrs(unittest.TestCase):
+
+    def test_get(self):
+        # Исправлена проверка ожидаемого значения
+        self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)  # Вернёт элемент со значением 2
+        self.assertEqual(arrs.get([], 0, "test"), "test")  # Вернёт значение по умолчанию
+
+    def test_slice(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
---
+++ utils/arrs.py
@@ -0,0 +1,46 @@
+"""Функции для работы с массивами"""
+
+
+def get(array, index, default=None):
+    """
+    Извлекает из списка значение по указанному индексу, если индекс существует.
+    Если индекс не существует, возвращает значение по умолчанию.
+    Функция работает только с неотрицательными индексами.
+    :param array: исходный список.
+    :param index: индекс извлекаемого элемента.
+    :param default: значение по-умолчанию.
+    :return: значение по индексу или значение по-умолчанию.
+    """
+
+    if 0 <= index < len(array):
+        return array[index]
+    return default
+
+
+def my_slice(coll, start=None, end=None):
+    """
+    Возвращает новый массив, содержащий копию части исходного массива.
+    :param coll: исходный список.
+    :param start: индекс, по которому начинается извлечение. Если индекс отрицательный,
+    start указывает смещение от конца списка. По умолчанию равен нулю.
+    :param end: индекс, по которому заканчивается извлечение (не включая элемент с индексом end).
+    Если индекс отрицательный, end указывает смещение от конца списка. По умолчанию равен длине исходного списка.
+    :return: массив элементов
+    """
+
+    length = len(coll)
+
+    if length == 0:
+        return []
+
+    if start is None:
+        normalized_start = 0
+    else:
+        normalized_start = start
+
+    if end is None or end > length:
+        normalized_end = length
+    else:
+        normalized_end = end
+
+    return coll[normalized_start:normalized_end]
